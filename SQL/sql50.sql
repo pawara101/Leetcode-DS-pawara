448e425fed577a136ddecaae42592db0068fc821
pawara101 <pawara101dassanayake@gmail.com>
2024-04-08 21:05:49 +0530

    SQl 50 test 20 done

--- SQL/sql50.sql
+++ SQL/sql50.sql
@@ -229,4 +229,80 @@ group by query_name
 --
 --1193. Monthly Transactions I
 
-SELECT DATE_FORMAT(trans_date, '%Y-%m') AS trans_month ,country as trans_count FROM Transactions
+"
+Transactions table:
++------+---------+----------+--------+------------+
+| id   | country | state    | amount | trans_date |
++------+---------+----------+--------+------------+
+| 121  | US      | approved | 1000   | 2018-12-18 |
+| 122  | US      | declined | 2000   | 2018-12-19 |
+| 123  | US      | approved | 2000   | 2019-01-01 |
+| 124  | DE      | approved | 2000   | 2019-01-07 |
++------+---------+----------+--------+------------+
+Output: 
++----------+---------+-------------+----------------+--------------------+-----------------------+
+| month    | country | trans_count | approved_count | trans_total_amount | approved_total_amount |
++----------+---------+-------------+----------------+--------------------+-----------------------+
+| 2018-12  | US      | 2           | 1              | 3000               | 1000                  |
+| 2019-01  | US      | 1           | 1              | 2000               | 2000                  |
+| 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
++----------+---------+-------------+----------------+--------------------+-----------------------+"
+
+SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month ,
+country ,
+COUNT(*) AS trans_count,
+SUM(IF(state = 'approved', 1, 0)) AS approved_count,
+SUM(amount) as trans_total_amount,
+SUM(IF(state = 'approved', amount, 0)) as approved_total_amount FROM Transactions
+GROUP BY month, country
+
+--1174. Immediate Food Delivery II
+"
+Delivery table:
++-------------+-------------+------------+-----------------------------+
+| delivery_id | customer_id | order_date | customer_pref_delivery_date |
++-------------+-------------+------------+-----------------------------+
+| 1           | 1           | 2019-08-01 | 2019-08-02                  |
+| 2           | 2           | 2019-08-02 | 2019-08-02                  |
+| 3           | 1           | 2019-08-11 | 2019-08-12                  |
+| 4           | 3           | 2019-08-24 | 2019-08-24                  |
+| 5           | 3           | 2019-08-21 | 2019-08-22                  |
+| 6           | 2           | 2019-08-11 | 2019-08-13                  |
+| 7           | 4           | 2019-08-09 | 2019-08-09                  |
++-------------+-------------+------------+-----------------------------+
+Output: 
++----------------------+
+| immediate_percentage |
++----------------------+
+| 50.00                |
++----------------------+
+
+delivery_id is the column of unique values of this table
+"
+
+
+SELECT CASE WHEN  `order_date` = `customer_pref_delivery_date` THEN "immediate"
+    ELSE "scheduled" END as status from Delivery
+"
+| status    |
+| --------- |
+| scheduled |
+| immediate |
+| scheduled |
+| immediate |
+| scheduled |
+| scheduled |
+| immediate |
+
+"
+
+Select 
+    round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
+from Delivery
+where (customer_id, order_date) in (
+  Select customer_id, min(order_date) 
+  from Delivery
+  group by customer_id
+);
+
+-- 550. Game Play Analysis IV
